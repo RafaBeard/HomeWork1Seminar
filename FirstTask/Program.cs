667d19b9de82c22646ec4f3e245bca964303b9b5
TRI <rafael.tagirzyanov@yandex.ru>
2022-08-25 15:50:04 +0300

    Первая задача

---
+++ FirstTask/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 < num2)
+{
+    Console.WriteLine("Max = "+num2);
+}
+else
+{
+    Console.WriteLine("Max = "+num1);
+}
